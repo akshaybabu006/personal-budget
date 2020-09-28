db243d32417397b6ba799cd31f518edd6ff8d7c7
Akshay Babu <akshaybabu@Akshays-MacBook-Pro.local>
2020-09-28 18:20:38 -0400

    Added server, Axios, and ChartJS

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use('/', express.static('public'));
+
+const budget = {
+    myBudget: [
+    {
+        title: 'Eat out',
+        budget:20
+    },
+    {
+        title: 'Rent',
+        budget:310
+    },
+    {
+        title: 'Groceries',
+        budget:110
+    },
+
+]};
+
+app.get('/hello',(req, res)=> {
+    res.send('Hello world !');
+});
+
+app.get('/budget', (req, res) => {
+    res.json(budget);
+});
+
+app.listen(port, () => {
+    console.log(`Serving at http://localhost:${port}`)
+
+    console.log(`app listening at http://localhost:${port}`)
+})
